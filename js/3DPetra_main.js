36d448c0b58287716342ebb7b01e40984aad69c8
Dillon Britt <dillonbritt@gmail.com>
2019-05-12 18:21:13 -0400

    addes stuff

--- js/3DPetra_main.js
+++ js/3DPetra_main.js
@@ -7,23 +7,18 @@ camera.position.set(0, 0, 10)
 //Controls
 var controls = new THREE.OrbitControls( camera );
 
+
+
 //Creating the Visual objects of Petra, Jordan
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 2, 0, 0 );
-var test = new THREE.BoxGeometry(30, 0, 0);
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-var cube2 = new THREE.Mesh( geometry, material );
-test = new THREE.Mesh( test, material);
+var base_column = new THREE.CircleGeometry(6, 5);
 
-
-var treasury_base = new THREE.BoxGeometry(5, 1, 10);
+var treasury_base = new THREE.BoxGeometry(5, .5, 20);
 var treasury_color = new THREE.MeshBasicMaterial( { color: 0x654321  } );
 treasury_base = new THREE.Mesh(treasury_base, treasury_color);
-
 
 var geo = new THREE.PlaneBufferGeometry(100, 100, 0, 0);
 
@@ -41,7 +36,6 @@ var plane = new THREE.Mesh(geo, mat);
 
 scene.add( treasury_base );
 plane.rotateX( - Math.PI / 2);
-cube2.position.set(10, 0, 0)
 
 //camera.position.z = 5;
 
