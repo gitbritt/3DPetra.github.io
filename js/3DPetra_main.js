534a5dc7f9e6bd1ba2faa01b9469257f1b3cd50d
Dillon Britt <dillonbritt@gmail.com>
2019-05-12 23:43:42 -0400

    removed a bunch of useless files, and added floor textures

--- js/3DPetra_main.js
+++ js/3DPetra_main.js
@@ -3,7 +3,10 @@
 var scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xffffff );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1, 1000 );
-camera.position.set(0, 0, 10)
+camera.position.set(10, 10, 20)
+
+
+
 //Controls
 var controls = new THREE.OrbitControls( camera );
 
@@ -14,7 +17,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 var treasury_color = new THREE.MeshBasicMaterial( { color: 0x654321  } );
-var base_column = new THREE.CylinderGeometry( 1, 1, 10, 1000);
+var base_column = new THREE.CylinderGeometry( 1, 1, 14, 1000);
 var base_column_1 = new THREE.Mesh(base_column, treasury_color);
 var base_column_2 = new THREE.Mesh(base_column, treasury_color);
 var base_column_3 = new THREE.Mesh(base_column, treasury_color);
@@ -24,31 +27,36 @@ var base_column_6 = new THREE.Mesh(base_column, treasury_color);
 
 var treasury_base = new THREE.BoxGeometry(5, .5, 30);
 treasury_base = new THREE.Mesh(treasury_base, treasury_color);
+var treasury_column_roof = new THREE.BoxGeometry(5, .5, 30);
+treasury_column_roof = new THREE.Mesh(treasury_column_roof, treasury_color);
 
 var geo = new THREE.PlaneBufferGeometry(100, 100, 0, 0);
 
-//Texture for the floor
-var texture = new THREE.TextureLoader().load( "" );
-texture.repeat.set(1, 1);
-texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
-////
+//Plane of existance
+var geometry = new THREE.PlaneGeometry( 100, 100, 10, 10 );
+geometry.rotateX( - Math.PI / 2 );
+var floorTexture = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/gitbritt/3DPetra.github.io/master/images/textures/ground_sand.jpg' );
+floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+floorTexture.repeat.set(20, 20);
+var material = new THREE.MeshBasicMaterial({map: floorTexture}),
+mesh = new THREE.Mesh( geometry, material );
+//scene.add( mesh );
 
 
 
 ////Adding Objects to the Visual world
-var mat = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
-var plane = new THREE.Mesh(geo, mat);
-base_column_1.position.set(0, 5, 14);
-base_column_2.position.set(0, 5, 8.5);
-base_column_3.position.set(0, 5, 4);
-base_column_4.position.set(0, 5, -4);
-base_column_5.position.set(0, 5, -8.5);
-base_column_6.position.set(0, 5, -14);
+treasury_column_roof.position.set(0, 14, 0);
+base_column_1.position.set(0, 7, 14);
+base_column_2.position.set(0, 7, 8.5);
+base_column_3.position.set(0, 7, 4);
+base_column_4.position.set(0, 7, -4);
+base_column_5.position.set(0, 7, -8.5);
+base_column_6.position.set(0, 7, -14);
 
-scene.add( treasury_base, base_column_1, base_column_2, base_column_3, base_column_4, base_column_5, base_column_6 );
+scene.add(mesh, treasury_base, treasury_column_roof , base_column_1, base_column_2, base_column_3, base_column_4, base_column_5, base_column_6 );
 
-plane.rotateX( - Math.PI / 2);
 
+////Allow movement
 var animate = function () {
     requestAnimationFrame( animate );
 
